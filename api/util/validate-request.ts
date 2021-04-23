d50fc5e6ce22eb15e05a263698b5332988e61c5c
harshgoel05 <harshgoel05@gmail.com>
2021-04-23 16:16:01 +0530

    ✨ Add Validation Middleware

---
+++ api/util/validate-request.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Response, Request } from "express";
+
+type RequestLocation = "query" | "body" | "params";
+
+export function validateRequest(location: RequestLocation, schema: any) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    let _location: any;
+    switch (location) {
+      case "query":
+        _location = req.query;
+        break;
+      case "body":
+        _location = req.body;
+        break;
+      case "params":
+        _location = req.params;
+        break;
+    }
+    try {
+      await schema.validate(_location);
+      next();
+    } catch (error) {
+      const message = error.errors[0];
+      return res.status(400).json({ error: message });
+    }
+  };
+}
